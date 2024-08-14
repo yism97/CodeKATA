581e9f8021834bbd7f0091add18fc0f441e8bb6a
yism97 <yihs97@naver.com>
2024-08-14 12:07:29 +0900

    [level 1] Title: 음양 더하기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,17 @@
+function solution(absolutes, signs) {
+    // 1. return 해야 하는 answer 생성
+    var answer = 0;
+    // 2. 두 배열을 비교해 가면서 값을 더하거나 빼줄 것
+    // 2-1. 두 배열은 크기가 같음
+    for (var i=0; i < absolutes.length; i++ ) {
+        // 부호(+, -) 에 따른 처리
+        if (signs[i] === true) {
+            // 값이 true 인 경우 양수
+            answer = answer + absolutes[i];
+        } else if(signs [i] === false) {
+            // 값이 false 인 경우 음수
+            answer = answer - absolutes[i];
+        }
+    }
+    return answer;
+}
