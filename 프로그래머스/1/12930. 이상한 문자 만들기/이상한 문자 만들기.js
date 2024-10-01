9083efa469d05bff93d5949f7e1e3228bcc3be60
yism97 <yihs97@naver.com>
2024-10-01 10:32:34 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -6,10 +6,11 @@ function solution(s) {
         // 이중 for문으로 각 인덱스의 문자열 길이 만큼 반복한다
         for(let j = 0; j < arr[i].length; j++){ 
              // 인덱스가 짝수일 경우 result 변수에 대문자로 변환해서 뒤에 붙이고, 그렇지 않으면 소문자를 붙인다
-            j % 2 === 0 ? answer += arr[i][j].toUpperCase() : answer += arr[i][j].toLowerCase();
+          answer+= (j % 2 === 0) ? arr[i][j].toUpperCase() : arr[i][j].toLowerCase();
 }
-        // 인덱스 번호가 arr 배열의 길이보다 작다면 공백을 붙인다
-        i < arr.length - 1 ? answer += ' ' : null;
+        // 인덱스 번호가 arr 배열의 길이보다 작다면  문자열 사이에 공백을 붙인다
+       // 배열을 탐색(0부터 시작)하면서 배열의 길이(1부터 시작)를 -1 한 것 보다 작다면 공백을 뒤에 붙여주기
+        i < arr.length - 1 ? answer += ' ' : answer += '';
     }
     return answer;
 }
