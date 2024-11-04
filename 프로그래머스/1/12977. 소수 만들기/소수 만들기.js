d27a68a5f7777683738e8c3cf68edbfd60b7d3ca
yism97 <yihs97@naver.com>
2024-11-04 09:31:47 +0900

    [level 1] Title: 소수 만들기, Time: 25.79 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,19 +1,24 @@
-function solution(nums) {
-    let answer = 0;
-    for(let i = 0; i < nums.length; i++) {
-        for(let j = i + 1; j < nums.length; j++) {
-            for(let k = j + 1; k < nums.length; k++) {
-                let sum = nums[i] + nums[j] + nums[k];
-                // 선택된 세 숫자의 합
-                if(sosu(sum)) answer++                 
-             }
+function primecheck(n){
+    for(var i=2;i<=Math.sqrt(n);i++){
+        if(n%i == 0){
+            return false;
         }
-    }      
-    return answer;
+    }
+    return true;    
 }
-let sosu = (sum) => {
-for (let i = 2; i < sum; i++){
-    if (sum % i === 0) return false;
-}
-    return true;
+function solution(nums){
+    var cnt = 0;
+    for(var i=0;i<nums.length;i++){
+        for(var j=i+1;j<nums.length;j++){
+            for(var w=j+1;w<nums.length;w++){
+                    //console.log(nums[i]+"/"+nums[j]+"/"+nums[w]);
+
+                    if(primecheck(nums[i]+nums[j]+nums[w])){
+                        //console.log(nums[i]+nums[j]+nums[w]);
+                        cnt++;
+                    }
+            }
+        }
+    }
+    return cnt;
 }
